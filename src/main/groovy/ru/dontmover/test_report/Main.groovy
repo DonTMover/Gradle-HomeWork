e1a3f5deed280382376d4c86d5fa8ad57a28106f
DonTMover <antantov624@gmail.com>
2024-08-19 23:31:44 +0300

    added groovy code

---
+++ src/main/groovy/ru/dontmover/test_report/Main.groovy
@@ -0,0 +1,10 @@
+class Main implements Plugin<Project> {
+    void apply(Project project) {
+        project.tasks.register('generateJacocoReport', JacocoTask) {
+            reports {
+                xml.enabled = true
+                html.enabled = true
+            }
+        }
+    }
+}
