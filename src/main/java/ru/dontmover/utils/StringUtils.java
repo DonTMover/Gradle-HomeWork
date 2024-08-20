fa4227039a5766b438737343bc5fc9fb446203b5
DonTMover <antantov624@gmail.com>
2024-08-20 14:02:35 +0300

    added some stylish

--- src/main/java/ru/dontmover/utils/StringUtils.java
+++ src/main/java/ru/dontmover/utils/StringUtils.java
@@ -2,13 +2,13 @@ package ru.dontmover.utils;
 
 public class StringUtils {
 
-        public static boolean isPositiveNumber(String str) {
-            try {
-                return Integer.parseInt(str) > 0;
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            return false;
+    public static boolean isPositiveNumber(String str) {
+        try {
+            return Integer.parseInt(str) > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return false;
+    }
 
 }
