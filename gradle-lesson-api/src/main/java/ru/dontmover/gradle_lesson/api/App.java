18cb2366cae1334e6b92896a6572ec252007ff18
DonTMover <antantov624@gmail.com>
2024-08-21 11:37:47 +0300

    awesome update

---
+++ gradle-lesson-api/src/main/java/ru/dontmover/gradle_lesson/api/App.java
@@ -0,0 +1,11 @@
+package ru.dontmover.gradle_lesson.api;
+
+
+import ru.dontmover.gradle_lesson.core.Utils;
+
+public class App {
+
+    public static void main(String[] args) {
+        Utils.isAllPositiveNumbers("12","79");
+    }
+}
---
+++ gradle-lesson-core/src/main/java/ru/dontmover/gradle_lesson/core/Main.java
@@ -0,0 +1,8 @@
+package ru.dontmover.gradle_lesson.core;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+}
